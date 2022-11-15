a477f1c0a8705b4ca84ebd3e0a89a3b24c6c6c1c
Krystian Kościelniak <krystian@koscielniak.pro>
2022-11-15 15:32:54 +0100

    ci: Add deployment to Goerli network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    hardhat: {
-      chainId: 1337,
+    goerli: {
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY!],
     },
   },
 };
